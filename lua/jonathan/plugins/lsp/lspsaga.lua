e13586cdd4b865de8ecc31654b519e0442fe43ac
Jonathan Morris <jonathanmorris@spotify.com>
2023-08-14 12:16:09 -0400

    Add auto-session

--- lua/jonathan/plugins/lsp/lspsaga.lua
+++ lua/jonathan/plugins/lsp/lspsaga.lua
@@ -1,21 +1,26 @@
 -- import lspsaga safely
 local saga_status, saga = pcall(require, "lspsaga")
 if not saga_status then
-  return
+	return
 end
 
 saga.setup({
-  -- keybinds for navigation in lspsaga window
-  scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
-  -- use enter to open file with definition preview
-  definition = {
-    keys = {
-      edit = "<CR>",
-    },
-  },
-  ui = {
-    colors = {
-      normal_bg = "#022746",
-    },
-  },
+	-- keybinds for navigation in lspsaga window
+	scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+	-- use enter to open file with definition preview
+	definition = {
+		keys = {
+			edit = "<CR>",
+		},
+	},
+	finder = {
+		keys = {
+			edit = "<CR>",
+		},
+	},
+	ui = {
+		colors = {
+			normal_bg = "#022746",
+		},
+	},
 })
