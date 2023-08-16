71002383cb076d98c06b7c37660c1054bf88bb31
Jonathan Morris <jonathanmorris@spotify.com>
2023-08-16 17:43:09 -0400

    Add keymaps for switching between buffers in history

--- lua/jonathan/core/keymaps.lua
+++ lua/jonathan/core/keymaps.lua
@@ -31,18 +31,12 @@ keymap.set("n", "<C-u>", "<C-u>zz")
 keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "Nzzzv")
 
--- keep previous yank when pasting over highlight
-keymap.set("x", "<leader>p", '"_dP')
-
 -- delete without losing previous yank
 keymap.set("n", "<leader>d", '"_d')
 keymap.set("v", "<leader>d", '"_d')
 
 -- disable Q
 keymap.set("n", "Q", "<nop>")
-
--- close all buffers except current
-keymap.set("n", "<leader>bd", ":%bd|e#<CR>")
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
@@ -59,8 +53,13 @@ keymap.set("n", "<leader>th", ":tabp<CR>") --  go to previous tab
 keymap.set("v", "<leader>y", '"+y')
 keymap.set("n", "<leader>yy", '"+yy')
 
--- copy relative path of current buffer
-keymap.set("n", "<leader>yp", ":Cppath<CR>")
+-------------
+-- Buffers --
+-------------
+keymap.set("n", "<leader>yp", ":Cppath<CR>") -- copy relative path of current buffer
+keymap.set("n", "<leader>bd", ":%bd|e#<CR>") -- close all buffers except current
+keymap.set("n", "<leader>n", ":bn<CR>") -- go to next buffer
+keymap.set("n", "<leader>p", ":bp<CR>") -- go to previous buffer
 
 ----------------------
 -- Plugin Keybinds
