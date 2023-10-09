4d7a40785f31e16cf563de0076fad9d9df4d6926
Jonathan Morris <musicmanmorris@gmail.com>
2023-10-09 16:33:10 -0400

    Add format on save for apex

--- lua/jonathan/core/options.lua
+++ lua/jonathan/core/options.lua
@@ -5,8 +5,8 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs and indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 
@@ -56,7 +56,25 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	group = "FileTypeGroup",
 })
 
-vim.lsp.set_log_level("debug")
+vim.api.nvim_create_autocmd("BufWritePre", {
+	-- inspired by https://www.reddit.com/r/neovim/comments/15tpl36/format_your_code_using_prettier_without_nullls/
+	desc = "Format apex files on save with prettier",
+	group = vim.api.nvim_create_augroup("PrettierApex", {}),
+	callback = function(opts)
+		if vim.bo[opts.buf].filetype == "apex" then
+			local clients = vim.lsp.get_active_clients()
+			if next(clients) == nil then
+				return nil
+			end
+			local fmt_command = "%!npx prettier --stdin-filepath %"
+			local cursor = vim.api.nvim_win_get_cursor(0)
+			vim.cmd(fmt_command)
+			-- In case formatting got rid of the line we came from.
+			cursor[1] = math.min(cursor[1], vim.api.nvim_buf_line_count(0))
+			vim.api.nvim_win_set_cursor(0, cursor)
+		end
+	end,
+})
 
 vim.api.nvim_create_user_command("Cppath", function()
 	local path = vim.fn.expand("%:p")
