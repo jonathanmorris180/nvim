c75c0df30dbd5b7a59b43be9d99837a584458bbd
Jonathan Morris <jonathanmorris@spotify.com>
2023-11-15 13:17:53 -0500

    Update copilot keymap

--- lua/jonathan/plugins/copilot.lua
+++ lua/jonathan/plugins/copilot.lua
@@ -24,7 +24,7 @@ return {
 				auto_trigger = true,
 				debounce = 75,
 				keymap = {
-					accept = "<Tab>",
+					accept = "<C-l>",
 					accept_word = false,
 					accept_line = false,
 					next = "<C-]>",
