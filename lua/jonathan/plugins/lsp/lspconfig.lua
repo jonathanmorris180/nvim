dc38329b0adf65ad7d617c2e13a27210965d2cab
Jonathan Morris <jonathanmorris@spotify.com>
2024-01-08 19:50:18 +0000

    Update .luarc.json

--- lua/jonathan/plugins/lsp/lspconfig.lua
+++ lua/jonathan/plugins/lsp/lspconfig.lua
@@ -131,12 +131,15 @@ return {
 							workspace = {
 								checkThirdParty = false,
 								library = {
-									vim.env.VIMRUNTIME,
+									vim.env.VIMRUNTIME .. "/lua",
 									"${3rd}/luv/library",
 									"${3rd}/busted/library",
 								},
 								-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
 								-- library = vim.api.nvim_get_runtime_file("", true),
+							},
+							diagnostics = {
+								globals = { "vim" },
 							},
 						},
 					})
