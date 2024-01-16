52d9b1adc1c6a436a13ecff69d12ce41787fe07b
Jonathan Morris <musicmanmorris@gmail.com>
2024-01-16 01:23:51 -0500

    Update salesforce.nvim

--- lua/jonathan/plugins/salesforce.lua
+++ lua/jonathan/plugins/salesforce.lua
@@ -4,10 +4,10 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
-	dev = true,
+	dev = false,
 	config = function()
 		require("salesforce").setup({
-			debug = true,
+			debug = false,
 			file_manager = {
 				ignore_conflicts = true,
 			},
@@ -16,10 +16,11 @@ return {
 
 		keymap.set("n", "<leader>se", "<cmd>SalesforceExecuteFile<cr>")
 		keymap.set("n", "<leader>sc", "<cmd>SalesforceClosePopup<cr>")
-		keymap.set("n", "<leader>stt", "<cmd>SalesforceExecuteCurrentMethod<cr>")
-		keymap.set("n", "<leader>stT", "<cmd>SalesforceExecuteCurrentClass<cr>")
+		keymap.set("n", "<leader>stm", "<cmd>SalesforceExecuteCurrentMethod<cr>")
+		keymap.set("n", "<leader>stc", "<cmd>SalesforceExecuteCurrentClass<cr>")
 		keymap.set("n", "<leader>sp", "<cmd>SalesforcePushToOrg<cr>")
 		keymap.set("n", "<leader>sr", "<cmd>SalesforceRetrieveFromOrg<cr>")
 		keymap.set("n", "<leader>sd", "<cmd>SalesforceDiffFile<cr>")
+		keymap.set("n", "<leader>so", "<cmd>SalesforceSetDefaultOrg<cr>")
 	end,
 }
