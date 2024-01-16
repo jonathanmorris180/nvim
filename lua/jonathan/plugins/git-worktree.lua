812f0cc7b1fe067dfc2f9946dbb3d6b13e242a51
Jonathan Morris <jonathanmorris@spotify.com>
2024-01-16 02:03:34 -0500

    Update salesforce.nvim

--- lua/jonathan/plugins/git-worktree.lua
+++ lua/jonathan/plugins/git-worktree.lua
@@ -4,8 +4,9 @@ return {
 	config = function()
 		local worktree = require("git-worktree")
 		worktree.on_tree_change(function(op, metadata)
-			print("metadata is " .. metadata.path)
+			print("worktree updated")
 			if op == worktree.Operations.Switch then
+				require("salesforce.org_manager"):get_org_info(true)
 				vim.fn.system("tmux-windowizer " .. metadata.path)
 				print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
 			end
