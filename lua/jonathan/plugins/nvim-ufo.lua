a1422b5d5f67bc732d5c973b625fc6233997d72f
Jonathan Morris <jonathanmorris@spotify.com>
2024-01-24 18:21:38 -0500

    Add nvim-ufo

---
+++ lua/jonathan/plugins/nvim-ufo.lua
@@ -0,0 +1,32 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = "kevinhwang91/promise-async",
+	config = function()
+		vim.o.foldcolumn = "1" -- '0' is not bad
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+		vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+		vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+		vim.keymap.set("n", "K", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				-- choose one of coc.nvim and nvim lsp
+				vim.lsp.buf.hover()
+			end
+		end)
+
+		require("ufo").setup({
+			provider_selector = function(_, _, _)
+				return {
+					"lsp",
+					"indent",
+				}
+			end,
+		})
+	end,
+}
