4c3f80d182c742cd7257b0251a5c0b9d92960fbd
Jonathan Morris <jonathanmorris@spotify.com>
2024-02-20 18:14:38 -0500

    Update to make sessions save with git-worktree

--- lua/jonathan/plugins/auto-session.lua
+++ lua/jonathan/plugins/auto-session.lua
@@ -2,10 +2,25 @@ return {
 	"rmagatti/auto-session",
 	lazy = true,
 	config = function()
-		print("starting auto-session")
+		vim.notify("starting auto-session")
 		require("auto-session").setup({
 			log_level = "error",
 			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			auto_save_enabled = true,
+			auto_restore_enabled = true,
+			auto_session_use_git_branch = true,
+			pre_save_cmds = {
+				"NvimTreeClose", -- helps nvim-tree work well with auto-session and git-worktree
+			},
+			post_restore_cmds = {
+				"NvimTreeOpen", -- helps nvim-tree work well with auto-session and git-worktree
+			},
+
+			cwd_change_handling = {
+				post_cwd_changed_hook = function()
+					require("lualine").refresh()
+				end,
+			},
 		})
 	end,
 	init = function()
--- lua/jonathan/plugins/git-worktree.lua
+++ lua/jonathan/plugins/git-worktree.lua
@@ -3,11 +3,16 @@ return {
 	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 	config = function()
 		local worktree = require("git-worktree")
+		worktree.setup({
+			clear_jumps_on_change = false, -- this is handled by auto-session, see https://github.com/ThePrimeagen/git-worktree.nvim/issues/13
+			update_on_change = false,
+		})
 		worktree.on_tree_change(function(op, metadata)
-			print("worktree updated")
+			vim.notify("Worktree updated")
 			if op == worktree.Operations.Switch then
+				vim.api.nvim_command("SessionRestore")
 				vim.fn.system("tmux-windowizer " .. metadata.path)
-				print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+				vim.notify("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
 			end
 		end)
 	end,
