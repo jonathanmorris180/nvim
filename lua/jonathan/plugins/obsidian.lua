d51158cf4dd65a9a0feffe2d1848081d590eade2
Jonathan Morris <jonathanmorris678@gmail.com>
2024-08-07 20:09:22 -0400

    Update obsidian plugin setup

--- lua/jonathan/plugins/obsidian.lua
+++ lua/jonathan/plugins/obsidian.lua
@@ -27,5 +27,98 @@ return {
 		ui = {
 			enable = false, -- prevents clashes with markdown.nvim
 		},
+		follow_url_func = function(url)
+			-- Open the URL in the default web browser.
+			vim.fn.jobstart({ "open", url }) -- Mac OS
+			-- vim.fn.jobstart({"xdg-open", url})  -- linux
+		end,
+		completion = {
+			-- Set to false to disable completion.
+			nvim_cmp = true,
+			-- Trigger completion at 2 chars.
+			min_chars = 2,
+		},
+
+		-- Optional, customize how note IDs are generated given an optional title.
+		---@param title string|?
+		---@return string
+		note_id_func = function(title)
+			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+			-- In this case a note with the title 'My new note' will be given an ID that looks
+			-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+			local suffix = ""
+			if title ~= nil then
+				-- If title is given, transform it into valid file name.
+				suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+			else
+				-- If title is nil, just add 4 random uppercase letters to the suffix.
+				for _ = 1, 4 do
+					suffix = suffix .. string.char(math.random(65, 90))
+				end
+			end
+			return tostring(os.time()) .. "-" .. suffix
+		end,
+
+		-- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
+		---@return string
+		image_name_func = function()
+			-- Prefix image names with timestamp.
+			return string.format("%s-", os.time())
+		end,
+
+		-- Optional, boolean or a function that takes a filename and returns a boolean.
+		-- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+		disable_frontmatter = false,
+
+		-- Optional, alternatively you can customize the frontmatter data.
+		---@return table
+		note_frontmatter_func = function(note)
+			-- Add the title of the note as an alias.
+			if note.title then
+				note:add_alias(note.title)
+			end
+
+			local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+			-- `note.metadata` contains any manually added fields in the frontmatter.
+			-- So here we just make sure those fields are kept in the frontmatter.
+			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+				for k, v in pairs(note.metadata) do
+					out[k] = v
+				end
+			end
+
+			return out
+		end,
+
+		-- Optional, for templates (see below).
+		templates = {
+			folder = "templates",
+			date_format = "%Y-%m-%d",
+			time_format = "%H:%M",
+			-- A map for custom variables, the key should be the variable and the value a function
+			substitutions = {},
+		},
+
+		-- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
+		open_app_foreground = true,
+
+		-- Specify how to handle attachments.
+		attachments = {
+			-- The default folder to place images in via `:ObsidianPasteImg`.
+			-- If this is a relative path it will be interpreted as relative to the vault root.
+			-- You can always override this per image by passing a full path to the command instead of just a filename.
+			img_folder = "assets/images", -- This is the default
+			-- A function that determines the text to insert in the note when pasting an image.
+			-- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+			-- This is the default implementation.
+			---@param client obsidian.Client
+			---@param path obsidian.Path the absolute path to the image file
+			---@return string
+			img_text_func = function(client, path)
+				path = client:vault_relative_path(path) or path
+				return string.format("![%s](%s)", path.name, path)
+			end,
+		},
 	},
 }
