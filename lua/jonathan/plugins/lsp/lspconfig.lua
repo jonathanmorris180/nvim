afe52f39a4ead2ecf7e3cd6083b58241d1f81420
Jonathan Morris <jonathanmorris678@gmail.com>
2024-09-08 16:52:49 -0400

    Migrate from tsserver to ts_ls

--- lua/jonathan/plugins/lsp/lspconfig.lua
+++ lua/jonathan/plugins/lsp/lspconfig.lua
@@ -2,7 +2,6 @@ return {
 	"neovim/nvim-lspconfig", -- in-built Neovim LSP
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp", -- autocompletion
-		"jose-elias-alvarez/typescript.nvim", -- additional functionality for typescript server (e.g. rename file & update imports)
 	},
 	config = function()
 		-- disable lsp logs ("off") unless needed so it doesn't create a huge file (switch to "debug" if needed)
@@ -10,12 +9,11 @@ return {
 
 		local lspconfig = require("lspconfig")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local typescript = require("typescript") -- additional functionality for tsserver (e.g. file renames & update imports)
 
 		local keymap = vim.keymap -- for conciseness
 
 		-- enable keybinds when lsp is available
-		local on_attach = function(client, bufnr)
+		local on_attach = function(_, bufnr)
 			-- keybind options
 			local opts = { noremap = true, silent = true, buffer = bufnr }
 
@@ -31,13 +29,6 @@ return {
 			keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
 			keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
 			keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
-
-			-- typescript specific keymaps (e.g. rename file and update imports)
-			if client.name == "tsserver" then
-				keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
-				keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports
-				keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables
-			end
 		end
 
 		-- used to enable autocompletion (assign to every lsp server config)
@@ -82,18 +73,22 @@ return {
 		})
 
 		-- configure typescript server with plugin
-		typescript.setup({
-			server = {
-				init_options = {
-					-- relative imports
-					preferences = {
-						importModuleSpecifierPreference = "relative",
-						importModuleSpecifierEnding = "minimal",
-					},
+		lspconfig["ts_ls"].setup({
+			init_options = {
+				-- relative imports
+				preferences = {
+					importModuleSpecifierPreference = "relative",
+					importModuleSpecifierEnding = "minimal",
 				},
-				capabilities = capabilities,
-				on_attach = on_attach,
+				plugins = {},
 			},
+			filetypes = {
+				"javascript",
+				"typescript",
+				"vue",
+			},
+			capabilities = capabilities,
+			on_attach = on_attach,
 		})
 
 		-- configure css server
