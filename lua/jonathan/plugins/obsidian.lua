b1bb2fc1df81a95f9cb467a878aa4834dc075ec7
Jonathan Morris <jonathanmorris678@gmail.com>
2025-01-30 14:36:05 -0500

    Update plugins

--- lua/jonathan/plugins/obsidian.lua
+++ lua/jonathan/plugins/obsidian.lua
@@ -2,7 +2,6 @@ return {
 	"epwalsh/obsidian.nvim",
 	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = true,
-	enabled = false, -- currently causing issues (probably conflicting with markdown.nvim)
 	ft = "markdown",
 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
 	-- event = {
