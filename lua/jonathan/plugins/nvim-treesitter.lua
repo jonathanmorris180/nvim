16bcb175f6fc90ce6cae3f1ed8999d4c00186c8c
Jonathan Morris <jonathanmorris678@gmail.com>
2026-07-10 01:20:32 -0400

    fix: nvim-treesitter errors with ensure_installed

--- lua/jonathan/plugins/nvim-treesitter.lua
+++ lua/jonathan/plugins/nvim-treesitter.lua
@@ -6,37 +6,6 @@ return {
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
-  config = function()
-    local treesitter = require("nvim-treesitter")
-    treesitter.setup({
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "apex",
-        "sosl",
-        "soql",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "graphql",
-        "bash",
-        "xml",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "go",
-      },
-      -- auto install above language parsers
-      auto_install = true,
-      -- ignore_install = { "latex" }, -- uncomment if needed for imap completions (mostly needed for maths) - see :h vimtex-faq-treesitter
-    })
-  end,
   init = function()
     -- see https://www.qu8n.com/posts/treesitter-migration-guide-for-nvim-0-12
     vim.api.nvim_create_autocmd("FileType", {
@@ -47,37 +16,5 @@ return {
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
     })
-
-    -- see https://www.qu8n.com/posts/treesitter-migration-guide-for-nvim-0-12
-    local ensureInstalled = {
-      "json",
-      "javascript",
-      "typescript",
-      "apex",
-      "sosl",
-      "soql",
-      "tsx",
-      "yaml",
-      "html",
-      "css",
-      "markdown",
-      "markdown_inline",
-      "graphql",
-      "bash",
-      "xml",
-      "lua",
-      "vim",
-      "dockerfile",
-      "gitignore",
-      "go",
-    }
-    local alreadyInstalled = require("nvim-treesitter").get_installed()
-    local parsersToInstall = vim
-      .iter(ensureInstalled)
-      :filter(function(parser)
-        return not vim.tbl_contains(alreadyInstalled, parser)
-      end)
-      :totable()
-    require("nvim-treesitter").install(parsersToInstall)
   end,
 }
